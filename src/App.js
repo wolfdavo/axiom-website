74a2d171fbc034bf0d5470d1b1e301168646ffc4
David Wolfenden <wolfdavo@gmail.com>
2021-06-05 23:27:29 -0500

    Re-wording

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           <li>Innovators</li>
         </ul>
         <p>Members of Axiom have a burning desire to write their own script and carve their own path. They do not settle, and they are never satisfied. They are catalysts of change and creation.</p>
-        <p>When you are accepted into Axiom, you are being welcomed into an ever-expanding and carefully selected network of exceptional people. You have a direct line to everyone in the syndicate. We do everything we can to support each other, creating a powerful interdependent synergy among members.</p>
+        <p>When you are accepted into Axiom, you are being welcomed into an ever-expanding and carefully selected network of exceptional people. You have a direct line to everyone in the syndicate. We do everything we can to support each other, creating a powerful network that you can leverage to do great things.</p>
         <p>Members frequently host events where you can network and connect in person. Lunch and coffee meetings, brainstorming sessions, group workouts, and celebrations/parties are all common.</p>
         <p>All members of Axiom are seen as equal. There is no hierarchy at all. When you are accepted, it means we have absolute trust in your character, values, and principles by which you live your life and that they align with those of the syndicate. Once accepted, you are free to invite and interview new members, host events, and network with anyone you want.</p>
         <p>There is no monetary cost associated with membership.</p>
@@ -35,7 +35,7 @@ function App() {
         <p>The first step of getting into Axiom is to fill out a written application. The application consists of questions regarding character, morals, motivations, achievements, self-discipline, struggles, goals, dreams, and is an overall dive into who you are and what you stand for. A referral by an Axiom member is a huge plus, but not required.</p>
 
         <p><b>Step Two - Remote interview with a member</b></p>
-        <p>Once we have read your written application and established a good fit for the syndicate, we will reach out and schedule a call with you. This is either a chat over the phone or a video call. It is casual, so don't stress. You can think of it as a networking opportunity, and the first taste of what is is like to be in Axiom. Your interviewer could be anyone in the syndicate.</p>
+        <p>Once we have read your written application and established a good fit for the syndicate, we will reach out and schedule a call with you. This is either a chat over the phone or a video call. It is casual, so don't stress. You can think of it as a networking opportunity, your interviewer could be anyone in the syndicate.</p>
 
         <p><b>Step Three - Attend an event</b></p>
         <p>After the call, you will be invited to attend an in-person event. If the nearest event is a long way away, you will be required to travel. This is the final step.</p>
